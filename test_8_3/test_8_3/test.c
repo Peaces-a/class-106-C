90896fdd4bf1e7a7e40975f26e5cd03865b949dc
Peaces-a <122713081+Peaces-a@users.noreply.github.com>
2023-08-03 09:55:53 +0800

    Add files via upload

---
+++ test_8_3/test_8_3/test.c
@@ -0,0 +1,43 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//左移操作符
+//int main()
+//{
+//	int a = 7 % 2;
+//	int b = 7 / 2;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 7;
+//	//左移操作符
+//	//移动的是二进制位
+//	int b = a << 1;
+//	printf("a=%d\n", a);
+//	printf("b=%d\n", b);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	/*float f = 3.14f;
+//	float f2 = f << 1;*/ //不支持移动float 浮点函数
+//	
+//	return 0;
+//}
+
+//右移操作符
+//int main()
+//{
+//	int a = -7;
+//	int b = a >> 1;
+//	printf("a=%d\n", a);
+//	printf("b=%d\n", b);
+//	return 0;
+//}
